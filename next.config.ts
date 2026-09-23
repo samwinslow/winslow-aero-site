fc527ca008b8a7272b4c397bd4371c8618fe498b
Sam Winslow <4645779+samwinslow@users.noreply.github.com>
2026-09-23 10:25:23 -0700

    try pure css approach

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/:path*",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: "script-src 'self'",
-          },
-        ],
-      },
-    ];
-  },
+  // async headers() {
+  //   return [
+  //     {
+  //       source: "/:path*",
+  //       headers: [
+  //         {
+  //           key: "Content-Security-Policy",
+  //           value: "script-src 'self'",
+  //         },
+  //       ],
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
